f07ab50ee8712e0943f9d8e5b42f50d20c7dd8c4
alisaidbas <bas.alisaid@gmail.com>
2023-08-04 18:56:25 +0200

    new-folder

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -15,13 +15,13 @@ def head():
 # and assign to the route of ('/')
 @app.route("/list")
 def header():
-    names =["Casper", "Sam", "Jasper"]
+    names =["Emre", "Numan", "Aysel", "Ali Said"]
     # numbers = range (1,11)
     return render_template("body.html", object = names)
 
 
 # run this app in debug mode on your local.
 if __name__== "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+     app.run(host='0.0.0.0', port=80)
 
