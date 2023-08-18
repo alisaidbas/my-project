4b65f419fd1b9660f56478a24c9b38b85a1193c6
alisaidbas <bas.alisaid@gmail.com>
2023-08-18 09:38:04 +0200

    added

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -17,10 +17,10 @@ def main_post():
     if request.method == 'POST':
         alpha = request.form['number']
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='Name', not_valid=True)
+            return render_template('index.html', developer_name='Ali Said', not_valid=True)
         number = int(alpha)
         if not 0 < number < 4000:
-            return render_template('index.html', developer_name='Name', not_valid=True)
+            return render_template('index.html', developer_name='Ali Said', not_valid=True)
         return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Name')
     else:
         return render_template('index.html', developer_name='Name', not_valid=False)
